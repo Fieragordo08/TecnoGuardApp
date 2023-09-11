3af4dc8ee5fed6362886d9618352221af048dd18
01OscarRam <oscarramirezalba012003@gmail.com>
2023-09-10 22:49:17 -0600

    Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,67 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hola Mundo!'),
+    return MaterialApp(
+      home: LoginPage(),
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        appBarTheme: AppBarTheme(
+          color: Colors.red, // Color de fondo de la AppBar (rojo)
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio de sesión'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Correo electrónico',
+                  icon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  icon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 24.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Agregar lógica de inicio de sesión aquí
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black, backgroundColor: Colors.yellow, // Color del texto del botón
+                ),
+                child: Text('Iniciar sesión'),
+              ),
+            ],
+          ),
         ),
       ),
     );
